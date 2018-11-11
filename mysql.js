82c09e81177c404a0fe1d9842f0b8a08a9eb8f91
Rafael Alves <ralphawk@gmail.com>
2018-11-11 17:09:43 -0200

    fix: usando banco remoto

--- mysql.js
+++ mysql.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql'),
-    /* connection = mysql.createPool({
+    connection = mysql.createPool({
         host     : 'sql141.main-hosting.eu',
         user     : 'u526894748_pic',
         password : process.env.MYSQL_PASS,
         database : 'u526894748_speed'
-    }); */
-    connection = mysql.createConnection({
+    });
+    /* connection = mysql.createConnection({
         host     : 'localhost',
         user     : 'root',
         password : "root",
         database : 'teste_speed'
-    });
+    }); */
 
 module.exports = connection;
