f3866b04b0e1baa71667f1392826e4c3a1b0e5b8
Rafael Alves <ralphawk@gmail.com>
2018-11-12 05:02:02 -0200

    fix: conexao mysql

--- mysql.js
+++ mysql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql'),
-    //connection = mysql.createPool(process.env.DATABASE_URL);
-    connection = mysql.createConnection({
+    connection = mysql.createPool(process.env.DATABASE_URL);
+    /* connection = mysql.createConnection({
         host     : 'localhost',
         user     : 'root',
         password : "root",
         database : 'teste_speed'
-    });
+    }); */
 
 module.exports = connection;
